af90c0b3fb7ea4f2cf746b85cb409593dd941cd1
Brandon Chartier <brandon.chartier@gmail.com>
2017-06-26 23:49:22 -0400

    updating version of coppice, adding ramda dependency

--- src/app/index.js
+++ src/app/index.js
@@ -1,26 +1,25 @@
 import { h, program } from 'coppice';
+import r from 'ramda';
 
-const model = {
-  count: 0,
-};
-
-const update = {
-  increment: (state) => ({ count: state.count + 1 }),
-  decrement: (state) => ({ count: state.count - 1 }),
-};
-
-const view = (state, msg) => (
-  <div>
-    <h1>{state.count}</h1>
-    <button onclick={msg.increment}>+</button>
-    <button onclick={msg.decrement} disabled={state.count <= 0}>-</button>
-  </div>
-);
+const count = r.over(r.lensProp('count'));
 
 const counter = program({
-  model,
-  update,
-  view,
+  model: {
+    count: 0
+  },
+  update: {
+    inc: _ => count(r.inc),
+    dec: _ => count(r.dec),
+    add: n => count(r.add(n))
+  },
+  view: msg => state => (
+    <div>
+      <h1>{state.count}</h1>
+      <button onclick={msg.inc}>+1</button>
+      <button onclick={msg.dec} disabled={state.count <= 0}>-1</button>
+      <button onclick={_ => msg.add(5)}>+5</button>
+    </div>
+  )
 });
 
 export default counter;
